5ba44e0fb247867b94baa02d080089948f842b56
nvpadmawar <nvpadmawar@gmail.com>
2021-05-28 15:47:29 +0530

    Division

---
+++ operation3.java
@@ -0,0 +1,19 @@
+class operation3{
+ pubblic static void main (string[] args){
+   
+  int a,b,c;
+   void div(){
+    
+   System.out.println("Enter a value : "+a);
+
+   System.out.println("Enter b value : "+b);
+
+   c=a/b;
+   System.out.println(" value of c: "+c);
+
+
+    }
+
+  }
+
+}
